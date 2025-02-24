038f9a38288b98205fb8a7782bc17c7fdec995e7
nicki <nickiiitadc4@gmail.com>
2025-02-24 20:13:19 +0100

    Ejercicio 8 repaso y los anteriores hechos , no te olvides de que los alter table siempre con el create stament !!!!!!!!!!

--- src/main/java/ejercicios/Exercises.java
+++ src/main/java/ejercicios/Exercises.java
@@ -1,11 +1,14 @@
 package ejercicios;
 
+import java.awt.Taskbar.State;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import org.checkerframework.checker.units.qual.s;
 
 public class Exercises {
 
@@ -119,7 +122,7 @@ public class Exercises {
         String query = "select alumnos.nombre, aulas.nombreAula from alumnos JOIN aulas ON aulas.numero = alumnos.codigo; ";
         try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(query);
-            System.out.println("-----------------------------------------");
+            System.out.println("---------------- -------------------------");
             while (resultSet.next()) {
                 System.out.printf("Student: %s Class: %s %n", resultSet.getString(1), resultSet.getString(2));
             }
@@ -128,6 +131,71 @@ public class Exercises {
         }
 
     }
+
+    /**
+     * Nombre de los alumnos, de las asignaturas y notas de aquellos alumnos que
+     * han aprobado alguna asignatura.
+     */
+
+    public void nombreAlumnoAprobado() {
+
+        String query = "SELECT alumnos.nombre, notas.NOTA, asignaturas.NOMBRE FROM alumnos JOIN notas ON notas.alumno = alumnos.codigo JOIN asignaturas ON notas.asignatura = asignaturas.COD WHERE notas.NOTA >= 5";
+
+        try (Statement st = connection.createStatement()) {
+
+            ResultSet resultSet = st.executeQuery(query);
+
+            System.out.println("---------------------");
+            while (resultSet.next()) {
+                System.out.printf("%s %d %s %n", resultSet.getString(1), resultSet.getInt(2),
+                        resultSet.getString("nombre"));
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void nombreAsignaturasSinAlumnos() {
+        String query = "SELECT asignaturas.NOMBRE FROM asignaturas WHERE asignaturas.COD NOT IN (SELECT notas.asignatura FROM notas);";
+
+        // .....repetir elstatement
+
+    }
+
+    public void nombreAlumnoPatronAltura(String pattern, int height) {
+
+        String query = "SELECT alumnos.nombre FROM ALUMNOS WHERE nombre like ? and altura>?";
+
+        try (PreparedStatement pst = connection.prepareStatement(query)) {
+            pst.setString(1, "%" + pattern + "%");
+            pst.setInt(2, height);
+            ResultSet resultSet = pst.executeQuery();
+            while (resultSet.next()) {
+                System.out.printf("Student: %s %n", resultSet.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void añadirNuevaColumna(String table, String nameField, String typeField, String description) {
+        String query = "ALTER TABLE ? add ? ? ? ";
+        try (PreparedStatement pst = connection.prepareStatement(query)) {
+            pst.setString(1, table);
+            pst.setString(2, nameField);
+            pst.setString(3, typeField);
+            pst.setString(4, description);
+            int rowsAffected = pst.executeUpdate();
+            System.out.println("Rows affected " + rowsAffected);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     
 
     public static void main(String[] args) {
@@ -135,11 +203,14 @@ public class Exercises {
         Exercises exercises = new Exercises();
         try {
             exercises.openConnection("add", "localhost", "root", "");
-            exercises.consultaAlumnos("a");
+            // exercises.consultaAlumnos("a");
             // exercises.darAltaAlumno("nicole", "diaz", 161, 21);
             // exercises.darAltaAsignatura(9, "Empresa");
             // exercises.darBajaAlumno(6);
-            exercises.nombreAulasConAlumnos();
+            // exercises.nombreAulasConAlumnos();
+            // exercises.nombreAlumnoAprobado();
+            // exercises.nombreAlumnoPatronAltura("nicole", 159);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
